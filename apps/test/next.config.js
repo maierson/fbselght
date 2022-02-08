65e7d35fc0aa2cae0c309ad5aa42cb3b722bac75
maierson <dahn@maierinc.com>
2022-02-08 15:11:07 -0500

    try deploy without getDoc

--- apps/test/next.config.js
+++ apps/test/next.config.js
@@ -1,20 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = withBundleAnalyzer({
+const nextConfig = {
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  distDir: ".dist",
-    target: "serverless",
-});
+  distDir: '.dist',
+  target: 'serverless',
+};
 
 module.exports = withNx(nextConfig);
